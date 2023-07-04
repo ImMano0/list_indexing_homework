755dc28375f70b191ad8d40e54dbab2cb6c5ebd8
ImMano0 <UrMa1371@gmail.com>
2023-07-04 21:18:39 +0500

    0007

--- List07.py
+++ List07.py
@@ -6,4 +6,24 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    if list1[0] == 1:
+        list1[0] = False
+    
+    if list1[1] == 1:
+        list1[1] = False
+    
+    if list1[2] == 1:
+        list1[2] = False
+    
+    if list1[3] == 1:
+        list1[3] = False
+    
+    if list1[4] == 1:
+        list1[4] = False
+
+    if list1[5] == 1:
+        list1[5] = False    
+
+    return list1
+
+print(main([1,2,3,4,5,6]))
